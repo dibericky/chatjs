80394d8983f4207b19995cd954efa14e68c7ee36
Riccardo Di Benedetto <riccardo.dibenedetto@mia-platform.eu>
2019-01-22 09:17:23 +0100

    Added front-end UI

---
+++ src/app/script.js
@@ -0,0 +1,69 @@
+const isTypingMap = {}
+$(function(){
+    var socket = io()
+
+    $("#formName").submit(e =>{
+        e.preventDefault()
+        const name = $("#name").val()
+        socket.name = name
+        socket.emit('newUser', {username:name})
+    })
+    
+
+    $('#formChat').submit(function(e){
+        e.preventDefault()
+        socket.emit('chat message', $('#msgInput').val())
+        addNewMessage(socket.name, $('#msgInput').val(), true)
+        $('#msgInput').val('')
+        return false
+    })
+    socket.on('checkName', obj =>{
+        if(obj.exists){
+            $("#name").focus()
+            $("#warning").text('Nome utente già in uso')
+        }else{
+            $("#nameDiv").fadeOut()
+            $("#chatDiv").fadeIn()
+        }
+    })
+    socket.on('chat message', msgObj =>{
+        console.log(msgObj)
+        addNewMessage(msgObj.username, msgObj.msg)
+    })
+    socket.on('newUser', obj=>{
+        newUserConnected(obj.username)
+    })
+    socket.on('isTyping', obj=>{
+        isTyping(obj.username)
+    })
+    socket.on('onlineUsersList', usersList => {
+        console.log(usersList)
+    })
+    $('#m').keypress(()=>{
+        socket.emit('isTyping')
+    })
+})
+
+function addNewMessage(username, msg, isMe){
+    let isMeClass = ''
+    if(isMe) isMeClass = "class='isMe'"
+    $('#messages').append($(`<li ${isMeClass}>`).text(`${username}: ${msg}`))
+    setEndTyping(username)
+}
+function newUserConnected(username){
+    $('#messages').append($('<li>').text(`${username} connected`))
+}
+function isTyping(username){
+    isTypingMap[username] = Date.now()
+    $("#isTyping").text(`${username} is typing...`)
+    setTimeout(()=>{
+        const timePassed = Date.now() - isTypingMap[username]
+        if(timePassed > 2000){
+            setEndTyping(username)
+        }
+    }, 3000)
+}
+function setEndTyping(username){
+    $("#isTyping").text('')
+    delete isTypingMap[username] 
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 const users = {}
 
 io.on('connection', function(socket){
-
+    
     socket.on('chat message', (msg)=>{
         socket.broadcast.emit('chat message', {
             username: socket.username,
@@ -19,18 +19,31 @@ io.on('connection', function(socket){
         sendOnlineUsersList(socket)
     })
     socket.on('newUser', (obj)=>{
-        socket.username = obj.username
-        users[socket.username] = 1
-        sendOnlineUsersList(socket)
-
-        socket.broadcast.emit('newUser', {
-            username: socket.username
-        })
+        const exists = alreadyExists(obj.username)
+        if(!exists){
+            socket.username = obj.username
+            users[socket.username] = 1
+            sendOnlineUsersList(socket)
+    
+            socket.broadcast.emit('newUser', {
+                username: socket.username
+            })
+        }
+        socket.emit('checkName', {exists: exists})
     })
+
+    socket.on('checkName', (obj) => {
+        socket.emit('checkName', {exists: alreadyExists(obj.username)})
+    })
+
     socket.on('isTyping', obj=>{
         socket.broadcast.emit('isTyping', {username: socket.username})
     })
 })
+
+function alreadyExists(username){
+    return Object.keys(users).includes(username)
+}
 
 function sendOnlineUsersList(socket){
     socket.broadcast.emit('onlineUsersList', users)
